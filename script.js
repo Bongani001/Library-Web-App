8e38bf0d3d101ef3b03d49261b692f78d329593c
Bongani001 <bbongs421@gmail.com>
2022-12-25 10:57:47 +0200

    Make Changes

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 let myLibrary = [];
 
 function Book() {
-    this.title = 'The Hobbit';
-    this.author = 'J.J.R Tolkien';
-    this.pages = 295;
-    this.read = 'not read yet';
-    this.info = function () {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-    };
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+   // this.info = function () {
+    //    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+    //};
 };
 
 function addBookToLibrary() {
     
-}
+};
 
-let book = document.getElementsByClassName()
 
-const theHobbit = new Book();
-console.log(theHobbit.info());
+
+let book = document.querySelector('.book');
+let containerCards = document.querySelector('.book-cards');
+
+
+//const theHobbit = new Book();
+//console.log(theHobbit.info());
+//myLibrary.push(theHobbit);
+console.log(myLibrary);
+
+let author = document.querySelector('#author');
+let title = document.querySelector('#title');
+let pages = document.querySelector('#pages');
+let read = document.querySelector('#read');
+
+function cool() {
+    book.textContent += `Title:`
+    book.textContent += `Auther:`
+    book.textContent += `Name:`
+    book.textContent += `Pages:`
+    containerCards.appendChild(book);
+};
+
+let submit = document.querySelector('#info');
+submit.addEventListener("click", cool);
+
+let formCompletion = document.querySelector('.form-completion');
+
